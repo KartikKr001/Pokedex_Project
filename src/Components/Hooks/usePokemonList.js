e8a656144266eca65b064be173122d7a8e125d53
Kartik Kumar <kartikkrprajapati01@gmail.com>
2024-07-10 17:39:18 +0530

    Changes 2

--- src/Components/Hooks/usePokemonList.js
+++ src/Components/Hooks/usePokemonList.js
@@ -1,86 +1,63 @@
 import axios from "axios";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
-
-function usePokemonList(url){
-    // const [ListOfPokemons,setListOfPokemons] = useState([]);
-    // const [isDownloadinsetPokemonListStates.g,SetDownloading] = useState(true);
-    // const [PokedexUrl,setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon');
-    
-    // const [nextUrl,setNextUrl] = useState('');
-    // const [prevUrl,setPrevUrl] = useState('');
-
-    const [pokemonListStates,setPokemonListStates] = useState({
-        ListOfPokemons:[],
-        isDownloading:true,
-        PokedexUrl:url,
-        nextUrl:"",
-        prevUrl:""
+function usePokemonList(url) {
+    const [pokemonListStates, setPokemonListStates] = useState({
+        ListOfPokemons: [],
+        isDownloading: true,
+        PokedexUrl: url,
+        nextUrl: "",
+        prevUrl: ""
     });
 
-    async function getResponse(){
-        // setDownloading(true)
+    async function getResponse() {
+        setPokemonListStates({ ...pokemonListStates, isDownloading: true });
 
-        // OR
-        setPokemonListStates({...pokemonListStates,isDownloading:true});
-        
-        const response = await axios.get(pokemonListStates.PokedexUrl);
-        console.log("response: ",response);
-        // array of 20 pokemons
-        const pokemonResults = response.data.results;
-        
-        /*setNextUrl(response.data.next);
-        setPrevUrl(response.data.previous);*/
-        setPokemonListStates((states)=>({
-            ...states,
-            nextUrl:response.data.next,
-            prevUrl:response.data.previous
-        }));
-        
-        // iterating array of pokemons ,and using their url to create array of promises
-        const pokemonResultsPromise = pokemonResults.map((pokemon)=>axios.get(pokemon.url));
-        
-        // passing all these to axios.all
-        const fin_data = await axios.all(pokemonResultsPromise);
-        // iterate on each pokemon,extract id ,name,image,,types
-        const res = fin_data.map((pokemon)=>{
-            const pokeData = pokemon.data;
-            return {
-                id:pokeData.id,
-                name:pokeData.name,
-                img:(pokeData.sprites.other) ? pokeData.sprites.other.dream_world.front_default:pokeData.sprites.front_shiny,
-                types:pokeData.types
-            }
-        });
-         
-        // setListOfPokemons(res);
-        // SetDownloading(false);
-        setPokemonListStates(prevState => ({
-            ...prevState,
-            ListOfPokemons: res,
-            isDownloading: false,
-        }));
+        try {
+            const response = await axios.get(pokemonListStates.PokedexUrl);
+            console.log("response: ", response);
+            
+            const pokemonResults = response.data.results;
+            setPokemonListStates((states) => ({
+                ...states,
+                nextUrl: response.data.next,
+                prevUrl: response.data.previous
+            }));
+            
+            const pokemonResultsPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
+            const fin_data = await axios.all(pokemonResultsPromise);
+
+            const res = fin_data.map((pokemon) => {
+                const pokeData = pokemon.data;
+                return {
+                    id: pokeData.id,
+                    name: pokeData.name,
+                    img: pokeData.sprites.other?.dream_world?.front_default || pokeData.sprites.front_shiny,
+                    types: pokeData.types
+                };
+            });
+
+            setPokemonListStates(prevState => ({
+                ...prevState,
+                ListOfPokemons: res,
+                isDownloading: false,
+            }));
+        } catch (error) {
+            console.error("Error fetching Pokémon data: ", error);
+            setPokemonListStates((prevState) => ({
+                ...prevState,
+                isDownloading: false,
+            }));
+        }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getResponse();
-    },[pokemonListStates.PokedexUrl]);
-    console.log("a",pokemonListStates)
-    return {pokemonListStates,setPokemonListStates};
+    }, [pokemonListStates.PokedexUrl]);
 
-    /*
+    console.log("Pokemon List States:", pokemonListStates);
 
-    const [x,setX] = useState(0);
-    const [y,setY] = useState(0);
-
-    useEffect(()=>{
-        console.log("re-rendering");
-       - ->   this array contains variables when those are changed , this function is re-rendered(if no array given function executed every time.   If empty arr given function executed 1 time only)
-       | 
-    },[x]);
-
-    */
+    return { pokemonListStates, setPokemonListStates };
 }
 
 export default usePokemonList;
